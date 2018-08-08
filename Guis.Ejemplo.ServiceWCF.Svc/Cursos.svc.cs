dd441fb6058b8e3220083f49db2ac9554e27fdc5
Luis <luischavez24@unmsm.edu.pe>
2018-08-07 23:47:58 -0500

    Primer commit del proyecto

---
+++ Guis.Ejemplo.ServiceWCF.Svc/Cursos.svc.cs
@@ -0,0 +1,29 @@
+﻿using Guis.Ejemplo.ServiceWCF.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Guis.Ejemplo.ServiceWCF.Svc
+{
+    public class Cursos : ICursos
+    {
+        public List<string> GetAllCursos()
+        {
+            var cursos = new List<string>
+            {
+                "Matematica",
+                "Física",
+                "Química",
+                "Razonamiento Matemático",
+                "Historia",
+                "Geografia"
+            };
+
+            return cursos;
+        }
+    }
+}
---
+++ Guis.Ejemplo.ServiciosWCF.Interface/ICursos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Guis.Ejemplo.ServiceWCF.Interface
+{
+    [ServiceContract]
+    public interface ICursos
+    {
+        [OperationContract]
+        [WebGet(
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            UriTemplate = "/Cursos/GetAllCursos")]
+        List<string> GetAllCursos();
+    }
+
+}
